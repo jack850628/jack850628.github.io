ee87fd377742087407393475f96d3a09302fed3c
奇幻 <30308931+jack850628@users.noreply.github.com>
2019-07-22 00:21:37 +0800

    移除多餘的async與await

--- make_a_fortune/_script.js
+++ make_a_fortune/_script.js
@@ -4,8 +4,8 @@
 var code_pattern = /(?<a>(?:發大財)+) (?<b>(?:發大財)+) (?<c>(?:發大財)+) (?<d>(?:發大財)+)/;//這段在Microsoft Edge會報Unexpected quantifier錯誤
 var make_a_fortune = '發大財';
 
-async function encode(str){
-	return await new Promise(resolve => {
+function encode(str){
+	return new Promise(resolve => {
 		setTimeout(() => {
 			let code = '';
 			for(let c of str){
@@ -21,8 +21,8 @@ async function encode(str){
 		});
 	});
 }
-async function decode(code){
-	return await new Promise(resolve => {
+function decode(code){
+	return new Promise(resolve => {
 		setTimeout(() => {
 			let str = '';
 			for(let c of code.split('\n')){
